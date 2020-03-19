4cb0f54b07ee422b5cc54fe14d7ce18306028627
Fernando Dingler <fdingler@amazon.com>
2020-03-18 17:35:46 -0700

    Upgrading dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'index.js'
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
@@ -14,39 +15,8 @@ module.exports = {
         loader: 'babel-loader'
       }
     ]
-  }
+  },
+  externals : {
+    react: 'commonjs react'
+  },
 };
-
-// const path = require('path');
-// module.exports = {
-//   entry: ["@babel/polyfill", './src/index.js'],
-//   mode: 'production',
-//   output: {
-//     path: path.resolve(__dirname, 'build'),
-//     filename: 'index.js',
-//     libraryTarget: 'commonjs2'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader']
-//       },
-//       {
-//         test: /\.js$/,
-//         include: path.resolve(__dirname, 'src'),
-//         exclude: /(node_modules|bower_components|build)/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env'],
-//             plugins: ['transform-class-properties']
-//           }
-//         }
-//       }
-//     ]
-//   },
-//   externals: {
-//     'react': 'commonjs react'
-//   }
-// };
