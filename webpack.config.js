f2bbe43ca44d2aaba17ca2a182840cc85ffb50bf
Fernando Dingler <fdingler@amazon.com>
2020-03-18 17:01:41 -0700

    Migrated to @babel 7 to upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,52 @@
 const path = require('path');
+
 module.exports = {
-  entry: ["babel-polyfill", './src/index.js'],
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js'
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-            plugins: ['transform-class-properties']
-          }
-        }
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
     ]
-  },
-  externals: {
-    'react': 'commonjs react'
   }
 };
+
+// const path = require('path');
+// module.exports = {
+//   entry: ["@babel/polyfill", './src/index.js'],
+//   mode: 'production',
+//   output: {
+//     path: path.resolve(__dirname, 'build'),
+//     filename: 'index.js',
+//     libraryTarget: 'commonjs2'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       },
+//       {
+//         test: /\.js$/,
+//         include: path.resolve(__dirname, 'src'),
+//         exclude: /(node_modules|bower_components|build)/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//             plugins: ['transform-class-properties']
+//           }
+//         }
+//       }
+//     ]
+//   },
+//   externals: {
+//     'react': 'commonjs react'
+//   }
+// };
